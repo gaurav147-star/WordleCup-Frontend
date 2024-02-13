3e8bc70a91e501791b88daa7041892c3262506a1
gaurav147-star <gauravgupta9158@gmail.com>
2024-02-13 16:54:59 +0530

    change for deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:8000");
+const socket = io.connect("https://wordlecup-backend.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
